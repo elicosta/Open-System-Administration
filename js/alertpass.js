60227c3ecd475d7f8d49ae8a91a1e6d1e9ff427f
Elivelton Costa <elivelton_costa@live.com>
2019-11-19 15:04:46 -0300

    fix bug
    - Utilization for user, adm and root.
    - Fix bug with auth and submit forms.

--- js/alertpass.js
+++ js/alertpass.js
@@ -1,23 +1,38 @@
 
 function alertpass()
 {
-    key = document.forms["form"]["keynew"].value;
-    key1 = document.forms["form"]["keyagain"].value;
-    senha = document.forms["form"]["keynow"].value;
+    if(document.forms["form"]["keynow"].value == "" && document.forms["form"]["keynew"].value == "" && document.forms["form"]["keyagain"].value == ""){
+        var adm = document.getElementById("adm");
+        key = document.forms["adm"]["keynew"].value;
+        key1 = document.forms["adm"]["keyagain"].value;
+
+        //email sendo chamado de senha
+        senha = document.forms["adm"]["user"].value;
+    }
+    else{
+        var form = document.getElementById("form");
+        key = document.forms["form"]["keynew"].value;
+        key1 = document.forms["form"]["keyagain"].value;
+        senha = document.forms["form"]["keynow"].value;
+    }
 
     if((key == "" && key1 == "" && senha == "") || (key == "" || key1 == "" || senha == "")){
-        alert("Algum campo em branco")
+        alert("Algum campo em branco");
     }
     else if (key != key1){
-        alert("Senha não coincidem")
+        alert("Senha não coincidem");
     }
     else{
         if (confirm("Você tem certeza em mudar a senha?")){
-            var form = document.getElementById("form");
-            form.submit();
+            if(document.forms["form"]["keynow"].value == "" && document.forms["form"]["keynew"].value == "" && document.forms["form"]["keyagain"].value == ""){
+                adm.submit();
+            }
+            else{
+                form.submit();
+            }
         }
         else{
-            window.location.replace("index.html")
+            window.location.replace("index.html");
         }
     }
 }
